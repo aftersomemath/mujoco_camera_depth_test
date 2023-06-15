bcce923e876fec5a5db56f39096ec598940fef3d
Levi Burner <leviburner@gmail.com>
2023-06-15 17:26:24 -0400

    Collect error over entire fov, optimize intrinsics

--- camera_depth_test.py
+++ camera_depth_test.py
@@ -1,3 +1,6 @@
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1' # Otherwise numpy spawns way too many threads
+
 import argparse
 import random
 import traceback
@@ -7,6 +10,12 @@ import mujoco
 import mujoco.viewer as viewer
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+
+import matplotlib.pyplot as plt
+import scipy
+
+import multiprocessing
+from multiprocessing import Pool
 
 # TODO read these from mjModel
 RES_X = 1280
@@ -57,15 +66,120 @@ def ogl_zbuf_negz_inv(zbuf, znear=None, zfar=None, C=None, D=None):
   zlinear = ogl_zbuf_projection_inverse(zbuf, C, D)
   return zlinear
 
-def run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D):
-  m = mujoco.MjModel.from_xml_path('./camera_depth_test.xml')
-  d = mujoco.MjData(m)
+def plot_errors(errors):
+  rows = len(errors)
+  cols = 4
 
-  view = viewer.launch_passive(m, d)
+  plt.figure()
+  for i, (name, error) in enumerate(errors.items()):
+      plt.subplot(rows, cols, 1 + i*4 + 0)
+      plt.title(name)
+      plt.plot(error[:, 0], error[:, 1])
+      plt.ylabel('mean')
+      plt.grid()
+      plt.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
+      if i == 3:
+        plt.xlabel('target z')
 
-  gl_ctx = mujoco.GLContext(RES_X, RES_Y)
-  gl_ctx.make_current()
+      plt.subplot(rows, cols, 1 + i*4 + 1)
+      plt.title(name)
+      plt.plot(error[:, 0], error[:, 2])
+      plt.ylabel('std dev')
+      plt.grid()
+      plt.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
+      if i == 3:
+        plt.xlabel('target z')
 
+      plt.subplot(rows, cols, 1 + i*4 + 2)
+      plt.title(name)
+      plt.plot(error[:, 0], error[:, 3])
+      plt.ylabel('min')
+      plt.grid()
+      plt.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
+      if i == 3:
+        plt.xlabel('target z')
+
+      plt.subplot(rows, cols, 1 + i*4 + 3)
+      plt.title(name)
+      plt.plot(error[:, 0], error[:, 4])
+      plt.ylabel('max')
+      plt.grid()
+      plt.ticklabel_format(style='sci', scilimits=(0,0), axis='y')
+      if i == 3:
+        plt.xlabel('target z')
+
+  # plt.tight_layout()
+  # plt.show()
+
+def objective_one_frame(x, optimization_dictionary):
+  RES_X = optimization_dictionary['RES_X']
+  RES_Y = optimization_dictionary['RES_Y']
+  pn_c  = optimization_dictionary['pn_c']
+  n_c   = optimization_dictionary['n_c']
+  znear = optimization_dictionary['znear']
+  zfar  = optimization_dictionary['zfar']
+  depth_hat     = optimization_dictionary['depth_hat']
+  # depth_hat_buf = optimization_dictionary['depth_hat_buf']
+
+  if 'C' in optimization_dictionary:
+    C = optimization_dictionary['C']
+    D = optimization_dictionary['D']
+  else:
+    C = None
+    D = None
+
+  fx, fy, cx, cy = x
+  cam_K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+
+  cam_x_over_z, cam_y_over_z = cv2.initInverseRectificationMap(
+          cam_K, # Intrinsics
+          None, # Distortion (0 for GPU rendered images)
+          np.eye(3), # Rectification
+          np.eye(3), # Unity rectification intrinsics (we want direction vector)
+          (RES_X, RES_Y), # Test all pixels in physical sensor
+          m1type=cv2.CV_32FC1)
+  cam_x_over_z = cam_x_over_z.astype(np.float64)
+  cam_y_over_z = cam_y_over_z.astype(np.float64)
+
+  depth_gt = (pn_c.T @ n_c) / (n_c[0] * cam_x_over_z + n_c[1] * cam_y_over_z + n_c[2])
+  depth_gt_buf = ogl_zbuf(depth_gt, znear, zfar)
+  # if C is not None:
+  #   depth_gt_buf_CD = ogl_zbuf(depth_gt, C=C, D=D)
+
+  # Assume all pixels on target
+  error     = depth_hat    - depth_gt
+  # error_buf = depth_hat_buf - depth_gt_buf
+  # if C is not None:
+  #   error_CD     = depth_hat_CD  - depth_gt
+  #   error_buf_CD = depth_hat_buf - depth_gt_buf_CD
+
+  return error.flatten()
+
+# TODO optimize C and D?
+def optimize_intrinsics(optimization_dictionaries, N):
+  args_i = np.round(np.linspace(0, len(optimization_dictionaries)-1, N)).astype(np.int64)
+  optimization_dictionaries_decimated = [optimization_dictionaries[i] for i in args_i]
+
+  with Pool(multiprocessing.cpu_count()) as p:
+    def objective(x):
+      residuals = []
+
+      args = [(x, d) for d in optimization_dictionaries_decimated]
+
+      residuals = p.starmap(objective_one_frame, args)
+      # residuals = [objective_one_frame(*a) for a in args]
+
+      residuals = np.array(residuals).flatten()
+      return residuals
+
+    # x0 = np.array([cam_K[0, 0], cam_K[1,1], cam_K[0, 2], cam_K[1, 2]])
+    x0 = optimization_dictionaries[0]['intrinsics']
+    result = scipy.optimize.least_squares(objective, x0=x0)
+  print(result)
+  print(result.x - x0, result.x, x0)
+  return result.x
+
+def collect_data(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D, intrinsics, view, m, d):
   scn = mujoco.MjvScene(m, maxgeom=100)
 
   # Turn on segmented rendering
@@ -105,18 +219,16 @@ def run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D):
   # https://developer.nvidia.com/content/depth-precision-visualized
   # https://www.lighthouse3d.com/tutorials/glsl-tutorial/rasterization-and-interpolation
 
-  fy = (RES_Y/2) / np.tan(yfov * np.pi / 180 / 2)
-  fx = fy
-  cx = (RES_X - 1) / 2.0 - 0.5 # Check this??
-  cy = (RES_Y - 1) / 2.0 - 0.5
+  if intrinsics is None:
+    # These 0.125's are close to what the intrinsics optimization usually returns
+    # Adding them makes the initial optimization much faster
+    fy = (RES_Y/2) / np.tan(yfov * np.pi / 180 / 2) - 0.00125
+    fx = fy - 0.00125
+    cx = (RES_X - 1) / 2.0 - 0.5 + 0.125
+    cy = (RES_Y - 1) / 2.0 - 0.5 + 0.125
+  else:
+    fx, fy, cx, cy = intrinsics
   cam_K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
-
-  # Debugging, build cam_x_over_z and cam_y_over_z from perspective matrix?
-  # camProject
-  # 1.3579951525e+00 0.0000000000e+00 0.0000000000e+00 0.0000000000e+00 
-  # 0.0000000000e+00 2.4142136574e+00 0.0000000000e+00 0.0000000000e+00 
-  # 0.0000000000e+00 0.0000000000e+00 5.0250887871e-03 -1.0000000000e+00 
-  # 0.0000000000e+00 0.0000000000e+00 1.1471571028e-01 0.0000000000e+00
 
   # Get the 3D direction vector for each pixel in the simulated sensor
   # in the format (x, y, 1)
@@ -132,20 +244,37 @@ def run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D):
 
   sample_list = []
 
+  error_list        = []
+  error_buf_list    = []
+  if C is not None:
+    error_CD_list     = []
+    error_buf_CD_list = []
+
+  optimization_dictionaries = []
+
   while True and view.is_running():
-    mujoco.mj_step(m, d)
-    view.sync()
-
-    test_pix = (int(RES_Y/2), int(RES_X/2))
-
     # Render the simulated camera
     mujoco.mjv_updateScene(m, d, vopt, pert, cam, mujoco.mjtCatBit.mjCAT_ALL, scn)
     mujoco.mjr_render(viewport, scn, ctx)
     image = np.empty((RES_Y, RES_X, 3), dtype=np.uint8)
-    depth = np.empty((RES_Y, RES_X, 1),    dtype=np.float32)
-    mujoco.mjr_readPixels(image, depth, viewport, ctx)
+    depth_hat_buf = np.empty((RES_Y, RES_X, 1),    dtype=np.float32)
+    mujoco.mjr_readPixels(image, depth_hat_buf, viewport, ctx)
+
     image = cv2.flip(image, 0) # OpenGL renders with inverted y axis
-    depth = cv2.flip(depth, 0) # OpenGL renders with inverted y axis
+    depth_hat_buf = cv2.flip(depth_hat_buf, 0) # OpenGL renders with inverted y axis
+
+    target_mujoco_id = None
+    for vgeom in scn.geoms:
+      if vgeom.objtype == mujoco.mjtObj.mjOBJ_GEOM:
+        name = mujoco.mj_id2name(m, mujoco.mjtObj.mjOBJ_GEOM,vgeom.objid)
+        if name == 'target':
+          target_mujoco_id = vgeom.segid
+    assert target_mujoco_id is not None
+
+    target_pixels = image[:, :, 0] == target_mujoco_id + 1
+    # cv2.imshow('target_pixels', target_pixels.astype(np.uint8) * 255)
+    # cv2.waitKey(1)
+    assert np.all(target_pixels)
 
     # Check XML reference, choice of zfar and znear can have big effect on accuracy
     zfar  = m.vis.map.zfar * m.stat.extent
@@ -157,17 +286,13 @@ def run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D):
     # cv2.waitKey(1)
 
     if d.time > 0.0:
-      depth = depth.astype(np.float64)
-      depth_hat_buf = depth[test_pix]
-      depth_linear    = ogl_zbuf_inv(depth, znear, zfar)
-      depth_hat = depth_linear[test_pix] # Center of screen
+      depth_hat_buf = depth_hat_buf.astype(np.float64)
+      depth_hat = ogl_zbuf_inv(depth_hat_buf, znear, zfar)
       if C is not None:
-        depth_linear_CD = ogl_zbuf_inv(depth, C=C, D=D)
-        depth_hat_CD = depth_linear_CD[test_pix]
+        depth_hat_CD = ogl_zbuf_inv(depth_hat_buf, C=C, D=D)
 
       # For visualization
       # depth_linear[depth_linear > m.vis.map.zfar - 0.0005] = 0 # Zero out depths farther than the z buffer
-
       # cv2.imshow('depth', depth_linear / np.max(depth_linear))
 
       # # Save off some pointclouds for visualization later
@@ -221,85 +346,152 @@ def run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D):
       # So we have
       # x_3 * [cam_x_over_z, cam_y_over_z, 1] n_c - pn_c^T n_c = 0
       # x_3 = (pn_c^T n_c) / ([cam_x_over_z, cam_y_over_z, 1] n_c)
-      depth_gt_plane = (pn_c.T @ n_c) / (n_c[0] * cam_x_over_z + n_c[1] * cam_y_over_z + n_c[2])
-      depth_gt = depth_gt_plane[test_pix] # Center of screen
-
-      # depth_gt = d.mocap_pos[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1]
+      depth_gt = (pn_c.T @ n_c) / (n_c[0] * cam_x_over_z + n_c[1] * cam_y_over_z + n_c[2])
       depth_gt_buf = ogl_zbuf(depth_gt, znear, zfar)
-      error_buf    = depth_hat_buf - depth_gt_buf
-      error = depth_hat - depth_gt
-
       if C is not None:
         depth_gt_buf_CD = ogl_zbuf(depth_gt, C=C, D=D)
-        # print('CD buf', depth_gt_buf_CD - depth_gt_buf, depth_gt_buf_CD, depth_gt_buf)
-        error_CD = depth_hat_CD - depth_gt
+
+      optimization_dictionary = {
+        'intrinsics': np.array([cam_K[0, 0], cam_K[1,1], cam_K[0, 2], cam_K[1, 2]]),
+        'RES_X': RES_X, 'RES_Y': RES_Y,
+        'pn_c': pn_c,
+        'n_c': n_c,
+        'znear': znear,
+        'zfar': zfar,
+        'depth_hat': depth_hat,
+      }
+
+      if C is not None:
+        optimization_dictionary['C'] = C
+        optimization_dictionary['D'] = D
+
+      optimization_dictionaries.append(optimization_dictionary)
+
+      # depth_gt = d.mocap_pos[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1]
+      
+      # Assume all pixels on target
+      error     = depth_hat    - depth_gt
+      error_buf = depth_hat_buf - depth_gt_buf
+      if C is not None:
+        error_CD     = depth_hat_CD  - depth_gt
         error_buf_CD = depth_hat_buf - depth_gt_buf_CD
-        # print('buf CD', error_buf_CD, depth_hat_buf, depth_gt_buf_CD)
 
-        sample_list.append(error_CD)
-      else:
-        error_CD = 0.0
-        error_buf_CD = 0.0
-        sample_list.append(error)
+      abs_error = np.abs(error)
+      error_list.append([
+        d.mocap_pos[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1],
+        np.mean(error),
+        np.std (error),
+        np.min (abs_error),
+        np.max (abs_error),
+      ])
 
-      print('znear {:0.8f} zfar {:0.1f} hat_buf {:0.2e} max_gt {:0.2f} est {:0.2f} gt {:0.2f} instant {:0.2e} instant CD {:0.2e} instant buf {:0.2e} instant buf CD {:0.2e} mean {:0.2e} std dev {:0.2e} max {:0.2e} min {:0.2e} N {:0.02e}'.format(
-        znear, zfar, depth_hat_buf, z_target_max, depth_hat, depth_gt,
-        error,
-        error_CD,
-        error_buf,
-        error_buf_CD,
-        np.mean(sample_list),
-        np.std(sample_list),
-        np.max(np.abs(sample_list)),
-        np.min(np.abs(sample_list)),
-        len(sample_list)))
+      abs_error_buf = np.abs(error_buf)
+      error_buf_list.append([
+        d.mocap_pos[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1],
+        np.mean(error_buf),
+        np.std (error_buf),
+        np.min (abs_error_buf),
+        np.max (abs_error_buf),
+      ])
 
-    target_mujoco_id = None
-    for vgeom in scn.geoms:
-      if vgeom.objtype == mujoco.mjtObj.mjOBJ_GEOM:
-        name = mujoco.mj_id2name(m, mujoco.mjtObj.mjOBJ_GEOM,vgeom.objid)
-        if name == 'target':
-          target_mujoco_id = vgeom.segid
-    assert target_mujoco_id is not None
+      if C is not None:
+        abs_error_CD = np.abs(error_CD)
+        error_CD_list.append([
+          d.mocap_pos[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1],
+          np.mean(error_CD),
+          np.std (error_CD),
+          np.min (abs_error_CD),
+          np.max (abs_error_CD),
+        ])
 
-    threshed = image[:, :, 0] == target_mujoco_id + 1
-    # cv2.imshow('threshed', threshed.astype(np.uint8) * 255)
-    # cv2.waitKey(1)
+        abs_error_buf_CD = np.abs(error_buf_CD)
+        error_buf_CD_list.append([
+          d.mocap_pos[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1],
+          np.mean(error_buf_CD),
+          np.std (error_buf_CD),
+          np.min (abs_error_buf_CD),
+          np.max (abs_error_buf_CD),
+        ])
+
+    mujoco.mj_step(m, d)
+
+    if view is not None:
+      view.sync()
 
     # Set the position of the moving target
+    if d.time * 2 > z_target_max - znear:
+      break
+
     next_target_depth = (d.time * 2) % (z_target_max - znear) + znear
     # next_target_depth = (d.time * 2) % (z_target_max - 0.9 * z_target_max) + 0.9 * z_target_max
     # next_target_depth = z_target_max
     d.mocap_pos[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1] = next_target_depth
 
-    # next_q_wp = R.from_rotvec(np.array([1.0, 0.0, 0.0]) * np.sin(d.time)).as_quat()
-    next_q_wp = R.from_rotvec(np.array([1.0, 0.0, 0.0]) * 0 * np.pi / 180.0).as_quat()
+    next_q_wp = R.from_rotvec(np.array([1.0, 0.0, 0.0]) * 45 * np.pi / 180.0).as_quat()
     d.mocap_quat[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][0]   = next_q_wp[3]
     d.mocap_quat[m.body_mocapid[mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, 'target')], :][1:4] = next_q_wp[0:3]
 
-  # End test
+  errors = {}
+  errors['error']        = np.array(error_list)
+  errors['error_buf']    = np.array(error_buf_list)
+  if C is not None:
+    errors['error_CD']     = np.array(error_CD_list)
+    errors['error_buf_CD'] = np.array(error_buf_CD_list)
+
+  return errors, optimization_dictionaries
+
+def run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D, intrinsics):
+  m = mujoco.MjModel.from_xml_path('./camera_depth_test.xml')
+  d = mujoco.MjData(m)
+
+  gl_ctx = mujoco.GLContext(RES_X, RES_Y)
+  gl_ctx.make_current()
+
+  view = viewer.launch_passive(m, d)
+  errors, optimization_dictionaries = collect_data(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D, intrinsics, view, m, d)
   view.close()
+
+  new_intrinsics = optimize_intrinsics(optimization_dictionaries, 10)
+
+  d = mujoco.MjData(m)
+  view = viewer.launch_passive(m, d)
+  new_errors, new_optimization_dictionaries = collect_data(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D, new_intrinsics, view, m, d)
+  view.close()
+
+  optimize_intrinsics(new_optimization_dictionaries, 10)
+
+  plot_errors(errors)
+  plot_errors(new_errors)
+  plt.show()
+
+  # np.savez('depth_test.npz', **errors)
+  # np.savez('depth_info.npz', dicts=optimization_dictionaries)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--z_buf_type', type=str, help='ogl_default, ogl_negz', required=True)
   args = parser.parse_args()
 
+  cv2.setNumThreads(1)
+
   # Accuracy degrades rapidly after the values in the Z buffer reach a certain point
   # Choose this at your discretion
+  z_max_buf_negz = (0.0035 / 2) + (0.0035 / 4) + (0.0035 / 8)
   if args.z_buf_type == 'ogl_default':
     ogl_zbuf     = ogl_zbuf_default
     ogl_zbuf_inv = ogl_zbuf_default_inv
-    z_max_buf = 0.993
-    C = -1.0100501776e+00
-    D = -2.2943142056e-01
+    z_max_buf = 1.0 - z_max_buf_negz
+    C = None #-1.0080322027e+00
+    D = None #-2.0080322027e-01
+    intrinsics = None #np.array([869.11688245,  869.12557739,  639.,         359.12569043])
   elif args.z_buf_type == 'ogl_negz':
     ogl_zbuf     = ogl_zbuf_negz
     ogl_zbuf_inv = ogl_zbuf_negz_inv
-    C = 5.0250887871e-03
-    D = 1.1471571028e-01
-    z_max_buf = 0.007
+    z_max_buf = z_max_buf_negz
+    C = None #4.0161013603e-03
+    D = None #1.0040161014e-01
+    intrinsics = None #np.array([869.11688245, 869.11653695, 639.        , 359.12489134])
   else:
     raise Exception('Unrecognized z_buf_type')
 
-  run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D)
+  run_test(ogl_zbuf, ogl_zbuf_inv, z_max_buf, C, D, intrinsics)
